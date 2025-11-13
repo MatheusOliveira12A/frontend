d6284ea9876bcdd760a958fa938ebca6525bc74e
matheusdofort123 <matheusdofort123@gmail.com>
2025-11-13 23:35:16 +0000

    atualizações

--- script.js
+++ script.js
@@ -2,18 +2,16 @@ const button = document.querySelector("button")
 const main = document.querySelector("main")
 button.addEventListener("click", buscarUsuarios)
 
- async function buscarUsuarios(){
-     const users = await fetch("http://localhost:3333").then((response)=> response.json())
-   
-     users.map(user => {
-        main.innerHTML += `
-        <section>
-            <h3>Nome: ${user.name}</h3>
-            <p>Idade: ${user.age} anos</p>
-            <p>E-mail: ${user.email}</p>
-        </section>
-        `
-     })
+async function buscarUsuarios() {
+  const users = await fetch("http://localhost:3333")
+    .then((response) => response.json())
 }
-
-// async / await
+  users.map(user => {
+    main.innerHTML += `
+      <section>
+        <h3>Nome: ${user.name}</h3>
+        <p>Idade: ${user.age}</p>
+        <p>E-mail: ${user.email}</p>
+      </section>
+    `
+  })
